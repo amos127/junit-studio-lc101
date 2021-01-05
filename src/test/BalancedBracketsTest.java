c3db32ea7b51ce9e80a163e1e80d787ec9ea4cea
amy sanders <amysanders89@gmail.com>
2021-01-04 22:48:32 -0600

    Completed studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,66 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void stringWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[string]"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("string"));
+    }
+
+    @Test
+    public void balancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void unbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void singleOpeningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Opener["));
+    }
+
+    @Test
+    public void singleClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Closer]"));
+    }
+
+    @Test
+    public void backwardsBracketBeforeOpeningShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    //This doesn't work if the brackets are equal in number but unbalanced
+    @Test
+    public void multipleBracketsUnbalancedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[]]]"));
+    }
+
+    @Test
+    public void balancedBracketsWithinStringShouldReturnTrue() {
+      assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void unbalancedBracketsWithinStringShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Un[[[balanced]!!!"));
+    }
 
 }
